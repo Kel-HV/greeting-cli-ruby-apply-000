068d28e72aac6fafae4d29b029c8c671f4eaac5a
rational-flag-9665 <kelvinherreravidals@gmail.com>
2020-09-27 03:15:32 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,6 @@
 # code the #greeting method here!
 
-ef greeting(name)
+def greeting(name)
   puts "Hello #{name}. It's nice to meet you."	  
   puts "Hello #{name.chomp.strip}. It's nice to meet you."
-  
-end 	
+end 
